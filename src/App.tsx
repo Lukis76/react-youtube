706df016c63d48920acb037deda4e6ec4eb06c5b
lukis76 <kiu.manoguerra@gmail.com>
2022-09-12 14:15:18 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+
+
+
+
 export const App = () => {
   return (
     <h1 className='font-bold text-indigo-900 grid place-content-center h-screen'>
-      toma toma
+      Hello world!
     </h1>
   )
 }
